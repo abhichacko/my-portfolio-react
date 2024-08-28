f9cfa94c23d53ae9c72b510b19c6670be7fb0764
abhichacko <itsmeabhilashthankachan@gmail.com>
2024-08-28 21:02:20 +0530

    vite config changes  eslint

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
-// https://vitejs.dev/config/
+import eslint from 'vite-plugin-eslint';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), eslint()],
+  server: {
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+});
